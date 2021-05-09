c8b1051c8eeccc2b7ae70fd176392a0b5d36afac
Quyt <buiquangquyet@gmail.com>
2021-05-09 10:56:34 +0700

    dev edit queue job

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -11,31 +11,23 @@ use Illuminate\Support\Facades\Http;
 class LogController extends Controller{
 
     public function index(){
+        $dateRange = '01/05/2021 - 05/05/2021';
+        $type = ['createOrder'];
+
         $response = Http::get('https://cpanel-shipping.kiotapi.com/api/setting/index', [
             'page' => 1,
-            'type'=>['createOrder']
+            'type' => $type,
+            'date-range'=> $dateRange
         ]);
 
         $response = json_decode($response->body());
         $loggings = $response->loggings;
         $data = $loggings->data;
-        if(!empty($data)){
-            foreach ($data as $key=>$value){
-                $log = new Order();
-                $log->type = 'createOrder';
-                $log->delivery_code = $value->delivery_code;
-                $log->shop_code = $value->shop_code;
-                $log->request_kv = isset($value->request_kv)?json_encode($value->request_kv,true):'';
-                $log->order_number = isset($value->response_kv->data->ORDER_NUMBER)?$value->response_kv->data->ORDER_NUMBER:'';
-                $log->response_kv = isset($value->response_kv)?json_encode($value->response_kv,true):'';
-                //$log->save();
-                //$this->dispatch(new InsertOrder());
-            }
+        $totalPage = $loggings->last_page;
+
+        for($i=1; $i<=$totalPage; $i++){
+            dispatch(new InsertOrder($i,$dateRange,$type));
         }
-        //dispatch(new InsertOrder());
-        //$this->dispatch(new ProcessPodcast);
-        dispatch(new SendEmail());
-            return;
         echo __METHOD__;die();
 
     }
--- app/Jobs/InsertOrder.php
+++ app/Jobs/InsertOrder.php
@@ -8,19 +8,45 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Http;
+use App\Models\Order;
 
 class InsertOrder implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable;
-
-    public function __construct()
+    private $page;
+    private $date;
+    private $ptypeage;
+    public function __construct($page,$date,$type)
     {
-
+        $this->page = $page;
+        $this->date = $date;
+        $this->type = $type;
     }
 
     public function handle()
     {
-        echo 121231233;
-        die();
+        $response = Http::get('https://cpanel-shipping.kiotapi.com/api/setting/index', [
+            'page' => $this->page,
+            'type'=> $this->type,
+            'date-range'=> $this->date
+        ]);
+
+        $response = json_decode($response->body());
+        $loggings = $response->loggings;
+        $data = $loggings->data;
+        if(!empty($data)){
+            foreach ($data as $key=>$value){
+                $log = new Order();
+                $log->type = 'createOrder';
+                $log->delivery_code = $value->delivery_code;
+                $log->shop_code = $value->shop_code;
+                $log->request_kv = isset($value->request_kv)?json_encode($value->request_kv,true):'';
+                $log->order_number = isset($value->response_kv->data->ORDER_NUMBER)?$value->response_kv->data->ORDER_NUMBER:'';
+                $log->response_kv = isset($value->response_kv)?json_encode($value->response_kv,true):'';
+                $log->save();
+            }
+        }
+        sleep(3);
     }
 }
